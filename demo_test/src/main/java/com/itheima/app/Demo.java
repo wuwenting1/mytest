eff88a263ab6745870aede23ee4e10ac95fa4166
wwt <wuwenting@itcast.cn>
2019-03-26 10:24:35 +0800

    java代码

--- demo_test/src/main/java/com/itheima/app/Demo.java
+++ demo_test/src/main/java/com/itheima/app/Demo.java
@@ -3,5 +3,9 @@ package com.itheima.app;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("111111111111111111111111");
+        System.out.println("111111111111111111111111");
+        System.out.println("111111111111111111111111");
+        System.out.println("111111111111111111111111");
+        System.out.println("111111111111111111111111");
     }
 }
